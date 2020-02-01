854192c172ac01946f667abe3a48781f536b43bf
ERERkin <erkin1399@gmail.com>
2020-02-01 19:10:52 +0600

    Update first task

--- src/main/java/com/MyServlet.java
+++ src/main/java/com/MyServlet.java
@@ -14,12 +14,23 @@ public class MyServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
         out.print("<table style=\"width:10%\">");
         int k = 0;
+        int l = 0;
         for(int i = 0; i < 10; i++){
             out.print("<tr>");
             for(int j = 0; j < 10; j++){
-                k++;
-                if((i + j) % 2 == 0) out.print("<th style=\"background-color: gray\">" + k + "</th>");
-                else out.print("<th>" + k + "</th>");
+                boolean check = true;
+                while (check) {
+                    l++;
+                    check = false;
+                    for (int h = 2; h < l; h++) {
+                        if(l % h == 0){
+                            check = true;
+                            break;
+                        }
+                    }
+                }
+                if((i + j) % 2 == 0) out.print("<th style=\"background-color: gray\">" + l + "</th>");
+                else out.print("<th>" + l + "</th>");
             }
             out.print("</tr>");
         }
